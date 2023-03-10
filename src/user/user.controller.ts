a5ab3e9d476d4e03e4fe08bceb9ffe0a56d186dc
Hieu Tran <hieutran@Hieus-Mac-mini.local>
2023-03-10 16:23:40 +0700

    decorator, chuan hoa code

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { User } from '@prisma/client';
+// import { AuthGuard } from '@nestjs/passport';
+// import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator';
+import { MyJwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
-    @UseGuards(AuthGuard('jwt'))
+    // @UseGuards(AuthGuard('jwt'))
+    @UseGuards(MyJwtGuard)
     @Get('user-info')
-    getUserInfo(@Req() request: Request) {
-        console.log(request.user)
-        return request.user
+    getUserInfo(@GetUser() user: User) {
+        return user
     }
 }
