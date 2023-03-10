f4089cb1e0245a87e8028cd8959b569806eb8764
Hieu Tran <hieutran@Hieus-Mac-mini.local>
2023-03-10 14:50:52 +0700

    private config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,11 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot({isGlobal:true}),
     AuthModule,
     UserModule,
     NoteModule,
-    PrismaModule,
-    ConfigModule
-    
+    PrismaModule    
   ]
 })
 export class AppModule { }
