9dad11047f5cef4fb24d94a46f3535bcca9eb365
Hieu Tran <hieutran@Hieus-Mac-mini.local>
2023-03-14 18:18:29 +0700

    refractor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { NoteModule } from './note/note.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 @Module({
@@ -9,7 +8,6 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({isGlobal:true}),
     AuthModule,
     UserModule,
-    NoteModule,
     PrismaModule    
   ]
 })
