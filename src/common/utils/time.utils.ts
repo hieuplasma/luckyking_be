c73cf57f3bf2cabadcff773e43f9c97271e89123
Hieu Tran <hieutran@Hieus-Mac-mini.local>
2023-03-22 17:27:05 +0700

    schedule draw for max3d + max3dplus, max3dpro, mega, power

--- src/common/utils/time.utils.ts
+++ src/common/utils/time.utils.ts
@@ -1,8 +1,24 @@
 export function getTimeToday(hour: number, minute: number) {
     const now = new Date();
     const today6am = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hour, minute, 0, 0);
-    // if (now.getTime() > today6am.getTime()) {
-    //     today6am.setDate(today6am.getDate() + 1);
-    // }
     return today6am
+}
+
+export function getNearestTimeDay(day: number, hour: number, minute: number) {
+    let currentDate = new Date();
+
+    // Get the day of the week (0-6), where 0 = Sunday, 1 = Monday, etc.
+    let currentDay = currentDate.getDay();
+    // Calculate the difference between the current day and 'day'
+    let diff = day - currentDay;
+
+    if (diff < 0) {
+        // If the difference is negative, add 7 to get the next Wednesday
+        diff += 7;
+    }
+
+    // Set the date to the next 'day' at 'hour':'minute'
+    let nextWednesday = new Date(currentDate.getTime() + (diff * 24 * 60 * 60 * 1000));
+    nextWednesday.setHours(hour, minute, 0, 0);
+    return nextWednesday
 }
--- src/result/result.service.ts
+++ src/result/result.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import { getTimeToday } from 'src/common/utils';
+import { LotteryType } from 'src/common/enum';
+import { getNearestTimeDay, getTimeToday } from 'src/common/utils';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { OldResultKenoDTO, OldResultMegaDTO, OldResultPowerDTO, OldResultMax3dDTO } from './dto';
 
@@ -24,21 +25,154 @@ export class ResultService {
     // - Quay thưởng Từ 18h00 – 18h30 Thứ 4 - Thứ 6 - Chủ nhật 
     // - Khi khởi tạo dự án, insert sẵn lịch quay cho 3 tuần tiếp theo 
     // - Lập lịch mỗi (5:00 AM Thứ hai) hàng tuần add schedule cho tuần tiếp theo (luôn đi trước 3 tuần)
+    @Cron('0 5 * * 1')
+    async addMegaDrawSchedule() {
+        const latestDraw = await this.prismaService.resultMega.findMany({
+            orderBy: {
+                drawCode: 'desc'
+            },
+            take: 1
+        });
+        let latestCode = latestDraw[0].drawCode
+        // thu 4
+        latestCode++
+        let newTime = getNearestTimeDay(3, 18, 0)
+        this.addScheduleMega(latestCode, newTime)
+        // thu 6
+        latestCode++
+        newTime = getNearestTimeDay(5, 18, 0)
+        this.addScheduleMega(latestCode, newTime)
+        // chu nhat
+        latestCode++
+        newTime = getNearestTimeDay(0, 18, 0)
+        this.addScheduleMega(latestCode, newTime)
+    }
+
+    private async addScheduleMega(drawCode: number, drawTime: Date) {
+        await this.prismaService.resultMega.create({
+            data: {
+                drawn: false,
+                drawCode: drawCode,
+                drawTime: drawTime
+            }
+        })
+    }
+
 
     // Power655:
     // - Quay thưởng Từ 18h00 – 18h30 Thứ 3 - Thứ 5 - Thứ 7
     // - Khi khởi tạo dự án, insert sẵn lịch quay cho 3 tuần tiếp theo 
     // - Lập lịch mỗi (5:00 AM Chủ Nhật) hàng tuần add schedule cho tuần tiếp theo (luôn đi trước 3 tuần) 
+    @Cron('0 5 * * 0')
+    async addPowerDrawSchedule() {
+        const latestDraw = await this.prismaService.resultPower.findMany({
+            orderBy: {
+                drawCode: 'desc'
+            },
+            take: 1
+        });
+        let latestCode = latestDraw[0].drawCode
+        // thu 3
+        latestCode++
+        let newTime = getNearestTimeDay(2, 18, 0)
+        this.addSchedulePower(latestCode, newTime)
+        // thu 5
+        latestCode++
+        newTime = getNearestTimeDay(4, 18, 0)
+        this.addSchedulePower(latestCode, newTime)
+        // thu 7
+        latestCode++
+        newTime = getNearestTimeDay(6, 18, 0)
+        this.addSchedulePower(latestCode, newTime)
+    }
+
+    private async addSchedulePower(drawCode: number, drawTime: Date) {
+        await this.prismaService.resultPower.create({
+            data: {
+                drawn: false,
+                drawCode: drawCode,
+                drawTime: drawTime
+            }
+        })
+    }
 
     // Max3d, Max3d+:
     // - Quay thưởng Từ 18h00 – 18h30 Thứ 2 - Thứ 4 - Thứ 6
     // - Khi khởi tạo dự án, insert sẵn lịch quay cho 3 tuần tiếp theo 
     // - Lập lịch mỗi (5:00 AM Thứ 7) hàng tuần add schedule cho tuần tiếp theo (luôn đi trước 3 tuần) 
+    @Cron('0 5 * * 6')
+    async addMax3dDrawSchedule() {
+        const latestDraw = await this.prismaService.resultMax3d.findMany({
+            where: {type: LotteryType.Max3D},
+            orderBy: {
+                drawCode: 'desc'
+            },
+            take: 1
+        });
+        let latestCode = latestDraw[0].drawCode
+        // thu 3
+        latestCode++
+        let newTime = getNearestTimeDay(1, 18, 0)
+        this.addScheduleMax3d(latestCode, newTime)
+        // thu 5
+        latestCode++
+        newTime = getNearestTimeDay(3, 18, 0)
+        this.addScheduleMax3d(latestCode, newTime)
+        // thu 7
+        latestCode++
+        newTime = getNearestTimeDay(5, 18, 0)
+        this.addScheduleMax3d(latestCode, newTime)
+    }
+
+    private async addScheduleMax3d(drawCode: number, drawTime: Date) {
+        await this.prismaService.resultMax3d.create({
+            data: {
+                drawn: false,
+                drawCode: drawCode,
+                drawTime: drawTime,
+                type: LotteryType.Max3D
+            }
+        })
+    }
 
     // Max3d Pro:
     // - Quay thưởng Từ 18h00 – 18h30 Thứ 3 - Thứ 5 - Thứ 7
     // - Khi khởi tạo dự án, insert sẵn lịch quay cho 3 tuần tiếp theo 
     // - Lập lịch mỗi (4:00 AM Thứ hai) hàng tuần add schedule cho tuần tiếp theo (luôn đi trước 3 tuần) 
+    @Cron('0 4 * * 0')
+    async addMax3dProDrawSchedule() {
+        const latestDraw = await this.prismaService.resultMax3d.findMany({
+            where: {type: LotteryType.Max3DPro},
+            orderBy: {
+                drawCode: 'desc'
+            },
+            take: 1
+        });
+        let latestCode = latestDraw[0].drawCode
+        // thu 3
+        latestCode++
+        let newTime = getNearestTimeDay(2, 18, 0)
+        this.addScheduleMax3dPro(latestCode, newTime)
+        // thu 5
+        latestCode++
+        newTime = getNearestTimeDay(4, 18, 0)
+        this.addScheduleMax3dPro(latestCode, newTime)
+        // thu 7
+        latestCode++
+        newTime = getNearestTimeDay(6, 18, 0)
+        this.addScheduleMax3dPro(latestCode, newTime)
+    }
+
+    private async addScheduleMax3dPro(drawCode: number, drawTime: Date) {
+        await this.prismaService.resultMax3d.create({
+            data: {
+                drawn: false,
+                drawCode: drawCode,
+                drawTime: drawTime,
+                type: LotteryType.Max3DPro
+            }
+        })
+    }
 
     // Keno:
     // - Quay thưởng 5p 1 lần, từ 6h04 sáng đến 21h54 tối
@@ -67,12 +201,9 @@ export class ResultService {
                     drawTime: newTime
                 }
             })
-            console.log('inserted '+ newTime)
             if (newTime.getTime() >= time21h54Today.getTime()) check = false
         }
     }
-
-
 
     async insertOldResultKeno(body: OldResultKenoDTO) {
         const result = this.prismaService.resultKeno.create({
