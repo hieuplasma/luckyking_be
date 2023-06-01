f1a5a6428db92aecbf2d0d8cd8f37117224b79d8
phudh3 <phudh@vietpointer.vn>
2023-06-01 10:53:03 +0700

    add approved field to result table

---
+++ prisma/migrations/20230526025644_add_approved_to_result/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "ResultKeno" ALTER COLUMN "displayId" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "ResultMax3d" ALTER COLUMN "displayId" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "ResultPower" ALTER COLUMN "displayId" DROP NOT NULL;
