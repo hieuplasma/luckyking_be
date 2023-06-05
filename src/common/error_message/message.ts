b7e146d531f2acdc38cc321327cfcc6b27e88883
phudh3 <phudh@vietpointer.vn>
2023-06-05 15:12:13 +0700

    add throw exception

--- src/common/error_message/message.ts
+++ src/common/error_message/message.ts
@@ -1,3 +1,5 @@
 export const errorMessage = {
-    BALANCE_NOT_ENOUGH: "Số dư không đủ"
+    BALANCE_NOT_ENOUGH: "Số dư không đủ",
+    NOT_FOUND: "Bản ghi không tồn tại",
+    UNAUTHORIZED: "Không thể có quyền"
 }
